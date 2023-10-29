a68ca0bef0524278a08b4000558735665fa0f75c
Sviatoslav Semochko <semosvyatko@gmail.com>
2023-10-29 20:43:43 +0200

    added more comments cleared "downloads" folder

--- main.py
+++ main.py
@@ -14,74 +14,88 @@ TOKEN = settings.bots.bot_token
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
+# greet user by his name
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {message.from_user.full_name}!")
 
 
+# Function to extract an archive
 async def extract_archive(archive_file, output_dir):
     if not os.path.exists(archive_file):
-        print(f"Архів {archive_file} не знайдено")
+        print(f"Archive {archive_file} not found")
         return
     try:
-        # Використовуємо patool для розархівування архіву
+        # using patool to unpack archive
         patoolib.extract_archive(archive_file, outdir=output_dir)
-        print(f"Архів {archive_file} розархівовано до {output_dir}")
+        print(f"Archive {archive_file} unzipped to {output_dir}")
     except Exception as e:
-        print(f"Помилка розархівування {archive_file}: {str(e)}")
+        print(f"Error unzipping {archive_file}: {str(e)}")
 
 
 async def process_files(input_dir, output_file):
+    # Open the output file for appending in UTF-8 encoding
     async with aiofiles.open(output_file, 'a', encoding='utf-8') as output:
+        # Iterate through the files in the input directory
         for filename in os.listdir(input_dir):
             input_file = os.path.join(input_dir, filename)
+            # Open the input file in binary mode to read its content
             with open(input_file, 'rb') as file:
                 data = file.read()
+                # Detect the encoding of the file's content using chardet
                 detected_encoding = chardet.detect(data)
+                # Check if the detected encoding is confident enough (confidence > 0.5)
                 if detected_encoding['confidence'] > 0.5:
                     encoding = detected_encoding['encoding']
+                    # Open the input file again with the detected encoding
                     async with aiofiles.open(input_file, 'r', encoding=encoding) as input:
                         lines = await input.readlines()
                         if lines:
                             first_line = lines[0]
-                            # Отримайте розширення (тип) файлу з імені файлу
+                            # Get the file extension (type) from the filename
                             file_extension = os.path.splitext(filename)[1]
-                            # Додайте розширення до першого рядка
+                            # Add the file extension to the first line
                             modified_first_line = f"{first_line.strip()} {file_extension}\n"
+                            # Write the modified line to the output file
                             await output.write(modified_first_line)
 
 
+# Function to handle incoming documents
 async def message_handler(message: Message) -> None:
     file_id = message.document.file_id
     file = await bot.get_file(file_id)
     file_path = file.file_path
     await bot.download_file(file_path, destination="C:/my_folder/arcihvebot/downloads/test.7z")
 
-    archive_file = "C:/my_folder/arcihvebot/downloads/test.7z"  # Змініть на ім'я вашого архіву
-    output_dir = "C:/my_folder/arcihvebot/downloads/"  # Каталог для розархівування файлів
-    output_file = 'C:/my_folder/arcihvebot/downloads/combined_output.txt'  # Файл, в який додається перший рядок з кожного файлу
+    archive_file = "downloads/test.7z"
+    output_dir = "downloads/"
+    output_file = 'downloads/combined_output.txt'
 
-    # Розархівуємо архів
+    # Extract the archive and process the files
     await extract_archive(archive_file, output_dir)
 
-    # Обробляємо файли, додаючи перший рядок та тип файлу до вихідного файлу
     await process_files(output_dir, output_file)
-
+    # Send the processed document back to the user
     await send_message_handler(message, bot)
 
 
+# send proccessed document back to user
 async def send_message_handler(message: Message, bot: Bot):
     txtfile = FSInputFile('downloads/combined_output.txt')
     await bot.send_document(message.chat.id, txtfile)
 
 
+# Main entry point
 async def main() -> None:
+    # Register message handlers for processing and sending messages
     dp.message.register(message_handler)
     dp.message.register(send_message_handler)
 
     await dp.start_polling(bot)
 
 
+# Start the bot when the script is run
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
